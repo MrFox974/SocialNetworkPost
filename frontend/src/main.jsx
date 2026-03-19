b9b72e49ee9e9045ab00e55a054987c75d750e2f
MrFox974 <lucas.soupa974@gmail.com>
2026-03-19 09:39:21 +0400

    CORS config update:

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,4 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// test
+
 createRoot(document.getElementById('root')).render(<App />)
